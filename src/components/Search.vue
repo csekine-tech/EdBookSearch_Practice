2fc045ed9e536fc4f786850bd34ce74f2c56ca4f
csekine-tech <csekine.mscll24@gmail.com>
2022-01-21 23:47:22 +0900

    基本設定

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  data: () => ({
+
+  }),
+  watch: {
+
+  },
+  mounted: function () {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
